00fec6516724ea7f33f037241770cc177df49967
KarolisDadura <33262517+KarolisDadura@users.noreply.github.com>
2017-11-08 20:52:47 +0200

    php variables

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
 
-echo "Hello world";
+$string = "labas";
+$number = 156;
+$real = 15.54;
+$boolean = true;
+$array = ["lt", "en", "de","$string","$number"];
+
+$object = (object)["key" => "value-1",
+					"key2" => "value-2",
+					"key3" => "value-3"
+				];
+
+// var_dump($string);
+// var_dump($number);
+// var_dump($real);
+// var_dump($boolean)
+
+var_dump($array[3]);
+var_dump($object -> key);
+
+//echo "<b>Hello world</b>";
